c14f94643a93e14e0a498059bb9b17d66121ead1
vernigora2k <vernigora2k@gmail.com>
2020-08-23 22:24:34 +0300

    v1.0.1temp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Display from './Display/Display'
 import Buttons from './Buttons/Buttons'
+import { useState } from 'react'
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState(0)
+
   return (
     <div className="App">
-      <Display/>
+      <Display currentNumber={ currentNumber }/>
       <Buttons/>
     </div>
   );
--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Display.css'
 
-export default function Display() {
+export default function Display({ currentNumber }) {
     return (
         <div className='display'>
+            <form className='dispalay__form'>
+                <input className='display__input' value={currentNumber}>
 
+                </input>
+            </form>
         </div>
     )  
 }
